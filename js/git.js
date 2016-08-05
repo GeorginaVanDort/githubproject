eb7c29b42da888175e3f4edc1eec758d518900fc
Georgina Van Dort <GeorginaVanDort@gmail.com>
2016-08-05 10:47:57 -0700

    allow user to search repo data - returns 30 repos to console

--- js/git.js
+++ js/git.js
@@ -3,14 +3,18 @@ var apiKey = require('./../.env').apiKey;
 function userProfile (username) {
   this.username = username;
 }
-console.log("HI22");
+
 userProfile.prototype.GitSearch = function () {
-  $.get('https://api.github.com/users/' + this.username + '?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + this.username + '/repos?page=2&per_page=200/?access_token=' + apiKey).then(function(response){
       console.log(response);
+      $("#userResults").text("")
+
+
+
+
     }).fail(function(error){
       console.log(error.responseJSON.message);
     });
   };
-
 
 exports.gitSearchModule = userProfile;
