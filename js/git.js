f0c80f7c5ace6a422997c09ef189a2a6b427938e
Georgina Van Dort <GeorginaVanDort@gmail.com>
2016-08-05 11:06:50 -0700

    print a list of repos to index.html

--- js/git.js
+++ js/git.js
@@ -7,7 +7,9 @@ function userProfile (username) {
 userProfile.prototype.GitSearch = function () {
   $.get('https://api.github.com/users/' + this.username + '/repos?page=2&per_page=200/?access_token=' + apiKey).then(function(response){
       console.log(response);
-      $("#userResults").text("")
+      for (var i = 0; i < response.length; i++) {
+        $("#userResults").append("<li>" + response[i].name + "</li>");
+      }
 
 
 
